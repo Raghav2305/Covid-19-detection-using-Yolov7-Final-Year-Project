b6df5d2c5ea52d4b9c3f34ddb680f7bfbdeaace2
Raghav Kavimandan <raghavkavimandan23@gmail.com>
2023-04-17 18:05:37 +0530

    added resnet and yolo, first cut

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, request
 from flask import render_template
+from keras.models import load_model
+import numpy as np
+from PIL import Image
 
 app = Flask(__name__, template_folder='templates')
+model = load_model("./Resnet50/resnet50.h5")
 
 
 @app.route("/")
@@ -22,6 +26,44 @@ def compare():
 @app.route("/impact")
 def impact():
     return render_template("impact.html")
+
+
+@app.route("/upload_files", methods=['POST'])
+def upload_files():
+    if 'image' not in request.files:
+        return 'No file part'
+    file = request.files['image']
+    print(file)
+    if file.filename == '':
+        return 'No selected file'
+
+    image = Image.open(file.stream)
+    print(image)
+    # preprocess the image
+    image = np.array(image.resize((224, 224))) / 255.0
+    # make predictions
+    predictions = model.predict(np.array([image]))
+    # get the predicted class
+
+
+# print the predicted class (assuming binary classification)
+    class_idx = np.argmax(predictions)
+    return redirect(url_for('predict', class_idx=class_idx))
+
+
+@app.route("/predict/<int:class_idx>")
+def predict(class_idx):
+    if class_idx == 0:
+        prediction = "COVID"
+    else:
+        prediction = "NORMAL"
+    return render_template('predict.html', prediction=prediction)
+
+    # class_idx = request.args.get('class_idx')
+    # if(class_idx == 0):
+    #     return "Covid"
+    # else:
+    #     return "Normal"
 
 
 if __name__ == "__main__":
---
+++ yolo.py
@@ -0,0 +1,40 @@
+import torch
+import cv2
+import numpy as np
+import torch
+import torchvision.models as models
+from torchvision import transforms
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+weights_path = "./best.pt"
+# Load the best weights from file
+model = torch.hub.load("WongKinYiu/yolov7", "custom",
+                       weights_path, trust_repo=True)
+model.to(device)
+# Set the model in evaluation mode
+model.eval()
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((640, 640)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# Prepare the input image
+image = cv2.imread('./test_on_these_images/Normal_2.jpeg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+results = model(image)
+class_labels = results.pandas().xyxy[0]['name'].tolist()
+print(class_labels)
+# input_image = transform(image).unsqueeze(0).to(device)
+
+
+# # Run the model on the input image
+# with torch.no_grad():
+#     outputs = model(input_image)
+
+# # Print the predicted bounding boxes and classes
+# print(outputs.xyxy[0])
